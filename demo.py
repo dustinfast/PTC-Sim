5eb9c9718861215934f835c1c6133432c0c46302
dustinfast <dustin.fast@hotmail.com>
2018-08-08 22:56:00 -0800

    Added mostly empty demo.py -DF

---
+++ demo.py
@@ -0,0 +1,9 @@
+""" demo.py - Starts the necessary services and runs the sim.
+"""
+
+if __name__ == '__main__':
+    try:
+        pass
+    except KeyboardInterrupt:
+        # do graceful shutdown
+        pass
